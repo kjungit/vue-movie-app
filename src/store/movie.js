b8a3329867b40830a443df9ed83356cde5654a89
kjun <kjun_all@naver.com>
2022-07-20 23:41:20 +0900

    Add Netlify functions

--- src/store/movie.js
+++ src/store/movie.js
@@ -69,10 +69,10 @@ export default {
             })
           }
         }
-      } catch (message) {
+      } catch ({ message }) {
         commit('updateState', {
           movies: [],
-          message,
+          message
         })
       } finally {
         commit('updateState', {
@@ -107,23 +107,6 @@ export default {
   }
 }
 
-function _fetchMovie(payload) {
-  const { title, type, year, page, id } = payload
-  const OMDB_API_KEY = '7035c60c'
-  const url = id 
-    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
-    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-
-  return new Promise((resolve, reject) => {
-    axios.get(url)
-    .then(res => {
-      if(res.data.Error) {
-        reject(res.data.Error)
-      }
-      resolve(res)
-    })
-    .catch(err => {
-      reject(err.message)
-    })
-  })
+async function _fetchMovie(payload) {
+  return axios.post('/.netlify/functions/movie', payload)
 }
